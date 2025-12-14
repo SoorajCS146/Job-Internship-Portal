b98b89b6e0718e968ee1fa91213f97603428f52a
Suvan77blr <sbumsrit61blr@gmail.com>
2025-12-14 13:01:18 +0530

    Added jakarta-validation-support to CompanyServiceRqstDto
    Signed-off-by: Suvan77blr <sbumsrit61blr@gmail.com>

--- company-service/src/main/java/com/jobportal/company_service/dto/CompanyRequestDto.java
+++ company-service/src/main/java/com/jobportal/company_service/dto/CompanyRequestDto.java
@@ -1,10 +1,26 @@
 package com.jobportal.company_service.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public class CompanyRequestDto {
+
+    @NotBlank
     public String companyName;
+
     public String industry;
+
+    @Size(max = 500)
     public String description;
+
+    @Pattern(regexp = "https?://.*")
     public String website;
+
+    @Email
+    @NotBlank
     public String contactEmail;
+
     public String contactPhone;
 }
